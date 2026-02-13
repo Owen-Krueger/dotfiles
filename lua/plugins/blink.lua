ddcf510fe7f8ae33a3db20ab6a4d3be83c6e328d
Owen Krueger <github@owenkrueger.dev>
2026-02-13 11:18:57 -0600

    Add easy-dotnet to blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -21,9 +21,16 @@ return { -- Autocompletion
     },
 
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev' },
+      default = { 'lsp', 'path', 'snippets', 'lazydev', 'easy-dotnet' },
       providers = {
         lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+        ['easy-dotnet'] = {
+          name = 'easy-dotnet',
+          enabled = true,
+          module = 'easy-dotnet.completion.blink',
+          score_offset = 10000,
+          async = true,
+        },
       },
     },
 
